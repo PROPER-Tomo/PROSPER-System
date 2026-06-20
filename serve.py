f4ce1cc8a2c30cdc48ff36d6e1d9552d4f53fb01
PROPER-Tomo <tomohiro.myose@supportbyprosper.com>
2026-06-20 17:45:22 +0900

    Create serve.py

---
+++ serve.py
@@ -0,0 +1,5 @@
+import os, http.server, socketserver
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+socketserver.TCPServer.allow_reuse_address = True
+with socketserver.TCPServer(("", 7788), http.server.SimpleHTTPRequestHandler) as h:
+    h.serve_forever()
